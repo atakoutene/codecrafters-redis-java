8519ae31f6a09d4d160b7bb64bf22421c4b65f4c
atakoutene <atakoutene@gmail.com>
2024-11-27 09:58:33 -0500

    Handle SET With Expiry 004

--- src/main/java/ProtocolParser.java
+++ src/main/java/ProtocolParser.java
@@ -58,7 +58,7 @@ public class ProtocolParser {
         logger.info("Handling SET command");
         String key = subCommands[3];
         String value = subCommands[5];
-        if (subCommands.length > 6 && subCommands[6].equalsIgnoreCase("EX")) {
+        if (subCommands.length > 6 && subCommands[6].equalsIgnoreCase("PX")) {
             long ttl = Long.parseLong(subCommands[7]);
             redisServer.setWithExpiry(key, value, ttl);
         } else {
