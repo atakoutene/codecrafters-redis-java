d739518d71f4e808479dcb669dbfa50dd515e5da
atakoutene <atakoutene@gmail.com>
2024-12-24 20:26:28 -0500

    Handle Propagation 006

--- src/main/java/ClientTask.java
+++ src/main/java/ClientTask.java
@@ -1,11 +1,13 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.logging.Logger;
 
 public class ClientTask implements Runnable {
-    private static final Logger logger = Logger.getLogger(ClientTask.class.getName());
     private final Socket clientSocket;
     private final Master master;
+    private static final Logger logger = Logger.getLogger(ClientTask.class.getName());
 
     public ClientTask(Socket clientSocket, Master master) {
         this.clientSocket = clientSocket;
@@ -14,26 +16,40 @@ public class ClientTask implements Runnable {
 
     @Override
     public void run() {
-        logger.info("Client task started for " + clientSocket.getRemoteSocketAddress());
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             OutputStream out = clientSocket.getOutputStream()) {
+        logger.info("Client task started for " + clientSocket.getInetAddress());
+        try (OutputStream out = clientSocket.getOutputStream();
+             InputStream in = clientSocket.getInputStream()) {
 
-            String line;
-            while ((line = in.readLine()) != null) {
-                logger.info("Received command: " + line);
-                String command = line.trim();
-                if (isWriteCommand(command)){
-                    logger.info("Write command: " + command);
+            byte[] bytes = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = in.read(bytes)) != -1) {
+                String command = new String(bytes, 0, bytesRead);
+                logger.info("Received command-fredy: " + command);
+                String response = ProtocolParser.parse(command, out);
+                if (response != null) {
+                    out.write(response.getBytes());
+                    out.flush();
+                }
+
+                if (isWriteCommand(command)) {
+                    logger.info("Propagating command to replicas: " + command);
                     master.propagateCommand(command);
                 }
-                out.write("+OK\r\n".getBytes());
-                out.flush();
             }
+
         } catch (IOException e) {
-            logger.severe("IOException: " + e.getMessage());
+            System.err.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.err.println("Failed to close socket: " + e.getMessage());
+            }
         }
     }
-
 
     private boolean isWriteCommand(String command) {
         return command.contains("SET") || command.contains("DEL");
