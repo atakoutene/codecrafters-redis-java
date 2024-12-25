5251f0f51fe9d94eb2c36006551d13e5de94cda7
atakoutene <atakoutene@gmail.com>
2024-12-24 22:22:38 -0500

    Handle Propagation 008 step info on replica 002

--- src/main/java/Replica.java
+++ src/main/java/Replica.java
@@ -21,52 +21,62 @@ public class Replica {
             serverSocket.setReuseAddress(true);
             logger.info("Replica started on port: " + port);
 
-            // Connect to the master
-            try (Socket masterSocket = new Socket(masterHost, masterPort);
-                 OutputStream out = masterSocket.getOutputStream();
-                 BufferedReader in = new BufferedReader(new InputStreamReader(masterSocket.getInputStream()))) {
+            // Create a new thread to connect to the master and handle all communication with it
+            new Thread(this::connectToMaster).start();
 
-                logger.info("Connected to master at " + masterHost + ":" + masterPort);
-
-                // Send PING command
-                out.write("*1\r\n$4\r\nPING\r\n".getBytes());
-                out.flush();
-                String response = in.readLine();
-                logger.info("Received response from master: " + response);
-
-                // Send REPLCONF listening-port command
-                String replconfListeningPort = String.format("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n", String.valueOf(port).length(), port);
-                out.write(replconfListeningPort.getBytes());
-                out.flush();
-                response = in.readLine();
-                logger.info("Received response from master: " + response);
-
-                // Send REPLCONF capa psync2 command
-                String replconfCapaPsync2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
-                out.write(replconfCapaPsync2.getBytes());
-                out.flush();
-                response = in.readLine();
-                logger.info("Received response from master: " + response);
-
-                // Send PSYNC command
-                String psyncCommand = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
-                out.write(psyncCommand.getBytes());
-                out.flush();
-                response = in.readLine();
-                logger.info("Received response from master: " + response);
-
-                // Process commands from master
-                while ((response = in.readLine()) != null) {
-                    logger.info("Received command from master: " + Arrays.toString(response.getBytes()));
-                    ProtocolParser.parse(response, out);
-                }
-
-            } catch (IOException e) {
-                logger.severe("Error connecting to master: " + e.getMessage());
+            // Accept client connections
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientTask(clientSocket, null)).start(); // Pass null as master since it's a replica
             }
 
         } catch (IOException e) {
             logger.severe("Error starting replica server: " + e.getMessage());
+        }
+    }
+
+    private void connectToMaster() {
+        try (Socket masterSocket = new Socket(masterHost, masterPort);
+             OutputStream out = masterSocket.getOutputStream();
+             BufferedReader in = new BufferedReader(new InputStreamReader(masterSocket.getInputStream()))) {
+
+            logger.info("Connected to master at " + masterHost + ":" + masterPort);
+
+            // Send PING command
+            out.write("*1\r\n$4\r\nPING\r\n".getBytes());
+            out.flush();
+            String response = in.readLine();
+            logger.info("Received response from master: " + response);
+
+            // Send REPLCONF listening-port command
+            String replconfListeningPort = String.format("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%d\r\n", String.valueOf(port).length(), port);
+            out.write(replconfListeningPort.getBytes());
+            out.flush();
+            response = in.readLine();
+            logger.info("Received response from master: " + response);
+
+            // Send REPLCONF capa psync2 command
+            String replconfCapaPsync2 = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+            out.write(replconfCapaPsync2.getBytes());
+            out.flush();
+            response = in.readLine();
+            logger.info("Received response from master: " + response);
+
+            // Send PSYNC command
+            String psyncCommand = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
+            out.write(psyncCommand.getBytes());
+            out.flush();
+            response = in.readLine();
+            logger.info("Received response from master: " + response);
+
+            // Process commands from master
+            while ((response = in.readLine()) != null) {
+                logger.info("Received command from master: " + Arrays.toString(response.getBytes()));
+                ProtocolParser.parse(response, out);
+            }
+
+        } catch (IOException e) {
+            logger.severe("Error connecting to master: " + e.getMessage());
         }
     }
 }
